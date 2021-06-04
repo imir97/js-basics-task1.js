584bc6c8a3fb4ea7477b330f67b5a330fe834864
unknown <you@example.com>
2021-06-04 12:18:47 +0200

    Add first file

---
+++ js-basics-task1.js
@@ -0,0 +1,9 @@
+let yourName = "Sonia Kuglin";
+let birthDate = new Date (1997,05,27);
+let num1 = 4;
+let num2 = 5;
+let num3 = 2;
+
+console.log(`Hello! This program has been written by ${yourName}`);
+console.log(`Author has been born on ${birthDate}`);
+console.log(`The result of hidden equation is: ${(num1 + num2) * num3}`);
